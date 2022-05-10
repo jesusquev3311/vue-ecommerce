908ecfbf9b7e286165ca1d3fc906f27934725144
Jesus Quevedo <jesus.quevedo@morningstar.com>
2022-05-09 22:26:17 -0400

    adding sass loader

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,7 +14,7 @@ module.exports = {
         'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
         'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
         indent: ['error', 4],
-        quotes: 'single',
+        quotes: ['error', 'single'],
     },
     overrides: [
         {
