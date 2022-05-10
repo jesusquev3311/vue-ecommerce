e3f2a5ba936f33a626b4b454b5fad37dcb919ecc
Jesus Quevedo <jesus.quevedo@morningstar.com>
2022-05-10 14:58:13 -0400

    adding productCard and Products components

---
+++ src/components/Products.vue
@@ -0,0 +1,29 @@
+<template>
+    <div style="padding: 25px;">
+        <div class="container">
+            <div class="columns">
+                <div class="column is-one-quarter" v-for="product in products" :key="product.id">
+                <ProductCard :product="product" />
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import { mapActions, mapGetters } from 'vuex';
+import ProductCard from './ProductCard.vue';
+
+export default {
+    components: { ProductCard },
+    mounted() {
+        this.getProducts();
+    },
+    computed: {
+        ...mapGetters('product', ['products']),
+    },
+    methods: {
+        ...mapActions('product', ['getProducts', 'addCart', 'removeCart']),
+    },
+};
+</script>
