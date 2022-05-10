a8fd87741a71146dc8d96e111e9af76091b57fe3
Jesus Quevedo <jesus.quevedo@morningstar.com>
2022-05-10 09:40:04 -0400

    restructuring vue files

---
+++ src/components/Base.vue
@@ -0,0 +1,45 @@
+<template>
+    <div>
+        <nav class="navbar navbar-expand-lg navbar-dark fixed-top">
+            <router-link class="navbar-brand" to="/">CS-eCommerce</router-link>
+            <div class="ml-auto">
+                <div v-if="user.photoURL">
+                    <img
+                        class="img-thumbnail profile-image"
+                        alt=""
+                        :src="user.photoURL"
+                    />
+
+                    <router-link class="btn btn-primary my-2 my-sm-0" to="/cart">
+                        <img
+                            src='https://pngimg.com/uploads/shopping_cart/shopping_cart_PNG38.png'
+                            width='50'
+                            alt=""
+                        />
+
+                        <span class="badge badge-danger badge-pill">{{ cart.length }}</span>
+                    </router-link>
+                </div>
+
+                <router-link v-else class="btn btn-primary my-2 my-sm-0" to="/login">
+                    Login
+                </router-link>
+            </div>
+        </nav>
+        <div class="page-container">
+            <router-view />
+        </div>
+    </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+
+export default {
+    name: 'Base-component',
+    computed: {
+        ...mapGetters('account', ['user']),
+        ...mapGetters('product', ['cart']),
+    },
+};
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Base/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+import Base from '@/components/Base.vue';
 
 export default {
     name: 'HomeView',
     components: {
-        HelloWorld,
+        Base,
     },
 };
 </script>
