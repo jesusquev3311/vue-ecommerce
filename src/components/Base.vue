f1093e9e2c2ed949ff38ed1ab57a3bd9461fafa4
Jesus Quevedo <jesus.quevedo@morningstar.com>
2022-05-10 14:13:23 -0400

    adding navbar to base

--- src/components/Base.vue
+++ src/components/Base.vue
@@ -1,31 +1,54 @@
 <template>
     <div>
-        <nav class="navbar navbar-expand-lg navbar-dark fixed-top">
-            <router-link class="navbar-brand" to="/">CS-eCommerce</router-link>
-            <div class="ml-auto">
-                <div v-if="user.photoURL">
-                    <img
-                        class="img-thumbnail profile-image"
-                        alt=""
-                        :src="user.photoURL"
-                    />
+        <nav class="navbar is-link" role="navigation" aria-label="main navigation">
+            <div class="navbar-brand">
+                <router-link class="navbar-item" to="/">CS-eCommerce</router-link>
+                <a
+                    role="button"
+                    class="navbar-burger"
+                    aria-label="menu"
+                    aria-expanded="false"
+                    data-target="navbarBasicExample"
+                >
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                </a>
+            </div>
 
-                    <router-link class="btn btn-primary my-2 my-sm-0" to="/cart">
-                        <img
-                            src='https://pngimg.com/uploads/shopping_cart/shopping_cart_PNG38.png'
-                            width='50'
-                            alt=""
-                        />
+            <div class="navbar-menu">
+                <div class="navbar-end">
+                    <div class="navbar-item">
+                        <div class="buttons">
+                            <div v-if="user.photoURL">
+                                <img
+                                    class="img-thumbnail profile-image"
+                                    alt=""
+                                    :src="user.photoURL"
+                                />
 
-                        <span class="badge badge-danger badge-pill">{{ cart.length }}</span>
-                    </router-link>
+                                <router-link class="button is-primary" to="/cart">
+                                    <img
+                                        src='https://pngimg.com/uploads/shopping_cart/shopping_cart_PNG38.png'
+                                        width='50'
+                                        alt=""
+                                    />
+
+                                    <span class="badge badge-danger badge-pill">
+                                        {{ cart.length }}
+                                    </span>
+                                </router-link>
+                            </div>
+
+                            <router-link v-else class="button is-primary" to="/login">
+                                Login
+                            </router-link>
+                        </div>
+                    </div>
                 </div>
-
-                <router-link v-else class="btn btn-primary my-2 my-sm-0" to="/login">
-                    Login
-                </router-link>
             </div>
         </nav>
+
         <div class="page-container">
             <router-view />
         </div>
@@ -43,3 +66,7 @@ export default {
     },
 };
 </script>
+
+<style lang="scss">
+    @import "@/assets/css/main.scss";
+</style>
